be78644066d43ad6616b72356680cf04b73775b4
Ano9xVIP <pallzxxx39@gmail.com>
2026-05-16 16:48:01 +0700

    Add files via upload

---
+++ api/VIP.js
@@ -0,0 +1,82 @@
+export default function handler(req, res) {
+
+const userAgent =
+req.headers['user-agent'] || ''
+
+const acceptHeader =
+req.headers['accept'] || ''
+
+res.setHeader(
+'Content-Type',
+'text/plain'
+)
+
+res.setHeader(
+'Access-Control-Allow-Origin',
+'*'
+)
+
+const isBrowser =
+
+acceptHeader.includes('text/html') ||
+
+userAgent.includes('Chrome') ||
+
+userAgent.includes('Firefox') ||
+
+userAgent.includes('Safari')
+
+if (isBrowser) {
+
+res.setHeader(
+'Location',
+'https://anonymous9xloader.vercel.app/'
+)
+
+res.status(302).send(`
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta http-equiv="refresh"
+content="0; url=https://anonymous9xloader.vercel.app/">
+
+<script>
+
+window.location.href =
+"https://anonymous9xloader.vercel.app/"
+
+</script>
+
+</head>
+
+<body>
+
+Redirecting...
+
+</body>
+
+</html>
+
+`)
+
+return
+}
+
+const luaCode = `
+
+print("═══════════════════════")
+print(" ANONYMOUS9X SYSTEM ")
+print("═══════════════════════")
+
+loadstring(game:HttpGet(
+"https://raw.githubusercontent.com/Ano9xVIP/NewLoaderVIP/refs/heads/main/NewLoaderVIP2.lua"
+))()
+
+`
+
+res.status(200).send(luaCode)
+}
